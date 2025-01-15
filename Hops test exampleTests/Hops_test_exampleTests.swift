84e561fd86f99f43c3abfb9fc1d720c0c0c4508d
Analu Jahi <ajjahi1208@gmail.com>
2025-01-15 13:26:34 -0500

    Initial Commit

---
+++ Hops test exampleTests/Hops_test_exampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hops_test_exampleTests.swift
+//  Hops test exampleTests
+//
+//  Created by Analu Jahi on 1/15/25.
+//
+
+import Testing
+@testable import Hops_test_example
+
+struct Hops_test_exampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
