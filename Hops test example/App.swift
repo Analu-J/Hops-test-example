ca3462a182dbca939a73e322b824f45d9183bdcd
Analu Jahi <ajjahi1208@gmail.com>
2025-01-27 15:32:40 -0500

    chnaged views

---
+++ Hops test example/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  Hops test example
+//
+//  Created by Analu Jahi on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct HopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Main()
+        }
+    }
+}
---
+++ Hops test example/Main.swift
@@ -0,0 +1,19 @@
+//
+//  Main.swift
+//  Hops test example
+//
+//  Created by Analu Jahi on 1/27/25.
+//
+
+import SwiftUI
+
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}
